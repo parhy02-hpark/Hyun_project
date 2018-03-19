43d7aa0120a44b89cb68a55176b16f772134916f
Hyunwoo Park <parhy02@gmail.com>
2018-03-18 22:25:50 -0700

    python file updated

--- myHello.py
+++ myHello.py
@@ -1,6 +1,16 @@
 def myprint(str):
   print str
 
+def mycalc(a,b):
+  print a+b
+  print a-b
+  print b-a
+  print a*b
+  print a/b
+  print b/a
+  print a//b
+  print b//a
+
 s = "Hello World!"
 myprint(s)
 s = "How are you doing?"
@@ -8,3 +18,6 @@ myprint(s)
 s = "I'm fine."
 myprint(s)
 
+a=5
+b=2
+mycalc(a,b)
